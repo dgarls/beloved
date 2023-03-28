497d443ebe7dd339ba42527f4ab6f459e19e3757
dgarls <dgarls.business@gmail.com>
2023-03-28 07:56:29 -0600

    working hard

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 panels = document.getElementsByClassName('panel')
 
 currentPanel = panels[0]
+
+const bringArrowDown = element => {
+    if (currentPanel === 0 && element.id === 'uparrow') return;
+    element.style.transform = "translateY(4vh)";
+}
+
+const bringArrowUp = element => {
+    if (currentPanel === panels.slice(-1) && element.id === 'downarrow') return;
+    element.style.transform = "translateY(-4vh)";
+}
+
+const moveDown = () => {
+    if ( currentPanel != panels.slice(-1) ){
+        window.scrollTo()
+    }
+}
+
+const moveUp = () => {
+    if (currentPanel != panels[0]){
+
+    }
+}
